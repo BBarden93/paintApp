d026d6c103d915ccac07976c58f66484f757be13
Brittany Barden <bbarden93@yahoo.com>
2018-03-15 17:27:25 -0700

    head

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
-const 
+ const 
     express = require('express'),
-    app = express()
-    server = require('http').Server(app)
+    app = express(),
+    server = require('http').Server(app),
     io = require('socket.io')(server)
 
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/public/index.html`)
 })
 
+io.on('connection', (socket) => {
+    console.log('A new client has connected...')
+    io.emit('new-user-connection', {boom: "Hello there!"})
 
+    socket.on('new-message', (draw) => {
+        io.emit('broadcast-message', draw)
+    })
+})
 
 server.listen(3000, (err) => {
     console.log(err || "Server running on Andre 3000")
